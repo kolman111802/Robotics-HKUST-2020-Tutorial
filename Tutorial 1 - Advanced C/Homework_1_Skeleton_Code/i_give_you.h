d452e332f4455606bd5cbaf8a5f1e589f9d3803b
kolman111802 <72688382+kolman111802@users.noreply.github.com>
2020-10-21 10:36:07 +0800

    Update i_give_you.h

--- Tutorial 1 - Advanced C/Homework_1_Skeleton_Code/i_give_you.h
+++ Tutorial 1 - Advanced C/Homework_1_Skeleton_Code/i_give_you.h
@@ -3,6 +3,7 @@
 #define SELECT_SAVED_DATA 'S'
 #define END_PROGRAM 'E'
 
+
 typedef enum {
     MONDAY,
     TUESDAY,
@@ -55,3 +56,74 @@ void invalid_input_msg(){
   printf("Invalid input!\n");
   printf("Please input your choice again: \n");
 }
+
+void validity(int d, int m, int y) {
+  if (d <= 0 || m <= 0 || m > 12 || y < 1800) {
+    invalid_input_msg();
+  }
+  if (d >= 31 && (m == 4 || m == 6 || m == 9 || m == 11)) {
+    invalid_input_msg();
+  }
+  if (d >= 29 && m == 2 && y % 4 != 0) {
+    invalid_input_msg();
+  }
+}
+void givendays(int d, int m) {
+  int ans = d*100 + m;
+  switch(ans) {
+    case 0101 : printf("First Day of January"); break;
+    case 2901 : printf("Birthday of the Great Tim Woo");break;
+    case 1003 : printf("Mario Day");break;
+    case 3103 : printf("World Backup Day");break;
+    case 1004 : printf("Whatever Day");break;
+    case 1304 : printf("Easter");break;
+    case 1105 : printf("National Technology Day");break;
+    case 0606 : printf("D-Day");break;
+    case 2512 : printf("Christmas");break;
+    case 2612 : printf("Boxing Day");break;
+    default: printf("Not a holiday.");
+  }
+}
+
+bool weekday(int d, int m, int y) {
+  /*int number = 0;
+  number = ((y - 1800) / 4 * 1461 + (y - 1800) % 4 * 365) ;
+  switch(m) {
+    case 1: number += 0; break;
+    case 2: number += 31; break;
+    case 3: number += 59; break;
+    case 4: number += 90; break;
+    case 5: number += 120; break;
+    case 6: number += 151; break;
+    case 7: number += 181; break;
+    case 8: number += 212; break;
+    case 9: number += 243; break;
+    case 10: number += 273; break;
+    case 11: number += 304; break;
+    case 12: number += 334; break;
+  }
+  if(y % 4 == 0) {
+    number++;
+  }
+  number += d;
+  printf("%i\n", number);
+  printf("%i", (number + 2) % 7);*/
+  int weekday  = (d += m < 3 ? y-- : y - 2, 23*m/9 + d + 4 + y/4- y/100 + y/400)%7;  
+  printf("Day: ");
+  switch(weekday) {
+    case 0: printf("Sunday"); break;
+    case 1: printf("Monday"); break;
+    case 2: printf("Tuesday"); break;
+    case 3: printf("Wednesday"); break;
+    case 4: printf("Thursday"); break;
+    case 5: printf("Friday"); break;
+    case 6: printf("Saturday"); break;
+  }
+   printf("\n"); 
+  if (weekday == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
